ea3527f3dfe318008acec67678680d610abff1fa
bilal-chajia <bildev23@gmail.com>
2018-09-07 17:24:45 +0100

    final version

--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -1,11 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-post-list-item',
   templateUrl: './post-list-item.component.html',
   styleUrls: ['./post-list-item.component.css']
 })
-export class PostListItemComponent implements OnInit {
+export class PostListItemComponent {
   @Input() postTitle: string;
   @Input() postContent: string;
   @Input() postLoveIts: number;
@@ -14,8 +14,6 @@ export class PostListItemComponent implements OnInit {
   constructor() {
   }
 
-  ngOnInit() {
-  }
 
   loveIt() {
     this.postLoveIts++;
--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,22 +1,15 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Post} from '../models/post';
-import {log} from 'util';
-
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent {
   @Input() posts: Post[];
 
 
   constructor() {
-    log(this.posts);
-  }
-
-  ngOnInit() {
-
   }
 
 }
